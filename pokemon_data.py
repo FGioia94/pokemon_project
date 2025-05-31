41738709d9e1fbca5d50618f79e17e09784d3bcb
FGioia94 <info@francescogioia.it>
2025-05-31 10:14:40 +0200

    added fetch and load pokemon data

--- pokemon_data.py
+++ pokemon_data.py
@@ -1,10 +1,21 @@
 import requests
 import json
+import os
 
 def fetch_data(pokemon):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-
     with open (f"{pokemon}.json", "w") as file:
         json.dump(response.json(), file, indent = 4)
 
-fetch_data("pikachu")
+def read_data(pokemon):
+    file_name = f"{pokemon}.json"
+    if not os.path.exists(file_name):
+        fetch_data(pokemon)
+
+    with open(file_name, "r") as file:
+        data = json.load(file)
+        return data
+
+
+data = read_data("ivysaur")
+print(data)
